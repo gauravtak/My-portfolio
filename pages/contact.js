d2428e1b2c6a4d1a0b77bf27c882f595b8e5387b
Gaurav Tak <gauravtak996@gmail.com>
2023-02-12 17:29:05 +0530

    scroll animation added in pages/index.js & pages/contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,22 +1,67 @@
 import styles from '@/styles/Home.module.css'
-import {FaLinkedin, FaTwitter} from "react-icons/fa"
-import {FaGithub} from "react-icons/fa"
-import {FaInstagram} from "react-icons/fa"
-import {FaLinkedIn} from "react-icons/fa"
+// import React from 'react';
+import { FaLinkedin, FaTwitter } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
+import { FaInstagram } from "react-icons/fa"
+import { FaLinkedIn } from "react-icons/fa"
+import { useEffect } from 'react';
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+// import { useLayoutEffect } from 'react';
 
-export default function Contact(){
-    return(
-     <div className='text-center'>
-        <h1 className='text-white py-4 m-20'>Contacts</h1>
-        <div className='flex py-16 px-7 gap-10  m-12 bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
-        <a href=""><FaTwitter/></a>
-        <a href=""><FaGithub/></a>
-        <a href=""><FaInstagram/></a>
-        <a href=""><FaLinkedin/></a>
 
-        {/* bg-gradient-to-tr from-red-500 via-pink-200 to-red-600 */}
-        
-      </div>
-     </div>
-    )
+const boxVariant = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  hidden: { opacity: 0, scale: 0 }
+}
+
+
+
+
+
+
+
+export default function Contact() {
+
+  const control = useAnimation()
+
+  const [ref, inView] = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    }
+    else {
+      control.start("hidden");
+    }
+  }, [control, inView])
+
+
+
+
+
+  return (
+    <div className=''>
+      <h1 className='text-white m-14'>Contacts</h1>
+      <motion.div ref={ref} variants={boxVariant} initial='hidden' animate={control} className='flex justify-center overflow-x-auto'>
+        <div className=' md:py-14 md:px-14 md:gap-10  md:m-10 sm:py-4 sm:px-4 sm:gap-4 sm:m-4  bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
+          <a href=""><FaTwitter size={50} /></a>
+        </div>
+        <div className=' md:py-14 md:px-14 md:gap-10  md:m-10 sm:py-4 sm:px-4 sm:gap-4 sm:m-4  bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
+          <a href=""><FaGithub size={50} /></a>
+        </div>
+        <div className=' md:py-14 md:px-14 md:gap-10  md:m-10 sm:py-4 sm:px-4 sm:gap-4 sm:m-4  bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
+          <a href=""><FaInstagram size={50} /></a>
+        </div>
+        <div className=' md:py-14 md:px-14 md:gap-10  md:m-10 sm:py-4 sm:px-4 sm:gap-4 sm:m-4  bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
+          <a href=""><FaLinkedin size={50} /></a>
+        </div>
+      </motion.div>
+
+
+      {/* bg-gradient-to-tr from-red-500 via-pink-200 to-red-600 */}
+
+
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,63 @@
 import styles from '@/styles/Home.module.css'
 import Projects from '../pages/projects.js'
 import Contact from "../pages/contact.js";
+import { useEffect } from 'react';
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion"
 
 // https://rxresu.me/gauravtak996/gaurav-tak //Resume link
 
+const boxVariant = {
+  visible: {opacity: 1, scale:1, transition: {duration: 0.7}},
+  hidden: {opacity:0 , scale: 0}
+}
+
 export default function Home() {
+
+  const control = useAnimation();
+
+  const [ref, inView] = useInView();
+
+  useEffect(()=>{
+    if(inView){
+      control.start("visible")
+    }
+    else{
+      control.start("hidden")
+    }
+
+  }, [control, inView]);
+
 
 
 
   return (
     <div className='text-center'>
 
-       <ul className='flex flex-row '>
-          <li className='ml-3 p-2 text-white' ><button href="#">Home</button></li>
-          <li className='ml-3 p-2 text-white' ><button href="#project">Projects</button></li>
-          <li className='ml-3 p-2 text-white'><button href="#">Contact</button></li>
-        </ul>
-     
+      <ul className='flex flex-row '>
+        <li className='ml-3 p-2 text-white' ><button href="#">Home</button></li>
+        <li className='ml-3 p-2 text-white' ><button href="#project">Projects</button></li>
+        <li className='ml-3 p-2 text-white'><button href="#">Contact</button></li>
+      </ul>
 
-      <div className='py-16 px-7  m-12 bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
+
+      <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control}  className='py-16 px-7  m-12 bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
         <img className='mx-auto w-40 rounded-full mb-10' src="https://www.meme-arsenal.com/memes/5ee99d558641255d6e670bbd54953397.jpg" alt="flower" />
         <h1 className='text-3xl font-bold'>Hi, This is Gaurav</h1>
         <p className='lg:px-40 mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, saepe quam quis porro aliquam, libero odio fugit eius doloremque earum inventore illum quae, laborum quos nesciunt incidunt veniam accusantium? Necessitatibus!</p>
         <button className=' mt-10 bg-green-400  rounded-xl text-lg text-black px-4 py-2'>Resume</button>
         {/* bg-gradient-to-tr from-red-500 via-pink-200 to-red-600 */}
 
-      </div>
+      </motion.div>
 
       {/* Projects Section */}
-      <Projects/>
-      <Contact/>
+      <div className=' min-h-[100vh]'>
+        <Projects />
+      </div>
+      <div className=' min-h-[80vh]'>
+        <Contact />
+      </div>
+
     </div>
 
 
