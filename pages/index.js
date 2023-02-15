eb6fd8193e3ca86563ca25a24fe4b4773a7d869d
Gaurav Tak <gauravtak996@gmail.com>
2023-02-15 16:24:40 +0530

    page navigation implemented and resume js created

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import Projects from '../pages/projects.js'
 import Contact from "../pages/contact.js";
 import { useEffect } from 'react';
 import { useInView } from "react-intersection-observer";
-import { motion, useAnimation } from "framer-motion"
+import { motion, useAnimation } from "framer-motion";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { FaTwitter } from "react-icons/fa"
+import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
 
 // https://rxresu.me/gauravtak996/gaurav-tak //Resume link
 
@@ -13,6 +17,8 @@ const boxVariant = {
 }
 
 export default function Home() {
+   
+  const router = useRouter();
 
   const control = useAnimation();
 
@@ -34,22 +40,21 @@ export default function Home() {
   return (
     <div>
       <div>
-        <ul className='flex flex-row '>
-          <li className='ml-3 p-2 text-white hover:text-fuchsia-400 font-extrabold text-[15px] lg:text-[20px]' ><button href="#">Home</button></li>
-          <li className='ml-3 p-2 text-white hover:text-fuchsia-400 font-extrabold text-[15px] lg:text-[20px]' ><button href="#project">Projects</button></li>
-          <li className='ml-3 p-2 text-white hover:text-fuchsia-400 font-extrabold text-[15px] lg:text-[20px]'><button href="#">Contact</button></li>
+        <ul className='flex flex-row justify-center '>
+          <Link href="/"><li className='ml-3 p-2 text-white hover:text-fuchsia-400 font-extrabold text-[15px] lg:text-[20px]' >Home</li></Link>
+          <Link href="/projects"><li className='ml-3 p-2 text-white hover:text-fuchsia-400 font-extrabold text-[15px] lg:text-[20px]' >Projects</li></Link>
+          <Link href="/contact"><li className='ml-3 p-2 text-white hover:text-fuchsia-400 font-extrabold text-[15px] lg:text-[20px]'>Contact</li></Link>
         </ul>
       </div>
 
 
-      <div className='text-center md:text-left min-h-[80vh]'>
-        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className=' py-16 px-7 m-12 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
-          <div className="lg:flex text-zinc-800 font-sans">
-            <img className=' shadow-2xl lg:mx-6 mx-auto w-40 rounded-full mb-10' src="https://www.meme-arsenal.com/memes/5ee99d558641255d6e670bbd54953397.jpg" alt="flower" />
-            <div className='lg:flex lg:flex-col'>
-              <h1 className='lg:mt-5 text-3xl lg:basis-1/2 font-bold'>Hi, This is Gaurav</h1>
-              <p className='lg:pr-40 lg:mb-10 mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, saepe quam quis porro aliquam, libero odio fugit eius doloremque earum inventore illum quae, laborum quos nesciunt incidunt veniam accusantium? Necessitatibus!</p>
-              <button className='transition-all ease-in-out shadow-xl rounded-xl hover:translate-y-2 duration-300 hover:scale-100 hover:rounded-3xl hover:bg-gradient-to-tr from-pink-400 to-red-400 mt-10 lg:m-0 lg:w-[120px] bg-gray-800 text-lg text-gray-200 px-4 py-2'>Resume</button>
+      <div className='flex text-center md:text-left min-h-[55vh]'>
+        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className='basis-[40%] py-12 px-10 m-10 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
+          <div className=" text-zinc-800 font-sans">
+            <img className=' shadow-2xl lg:mx-4 mx-auto w-20 rounded-full' src="https://www.meme-arsenal.com/memes/5ee99d558641255d6e670bbd54953397.jpg" alt="flower" />
+            <div className='lg:flex ml-5 lg:flex-col'>
+              <h1 className='lg:mt-5 text-3xl font-bold'>Hi, This is Gaurav</h1>
+              <p className='lg:pr-40 lg:mb-4 mt-4'>A Frontend Web Developer</p>
             </div>
 
           </div>
@@ -58,12 +63,40 @@ export default function Home() {
           {/* */}
 
         </motion.div>
+        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className=' py-10 px-12 my-10 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
+          <div className=" text-zinc-800 font-sans">
+            <div className='lg:flex ml-2 lg:flex-col'>
+              <HiOutlineClipboardDocumentList size={80}/>
+              <h1 className='lg:mt-5 mb-5 text-3xl font-bold'>See My Resume</h1>
+              
+              <button type='button' onClick={()=> router.push('/resume')} className='transition-all ease-in-out shadow-xl rounded-xl hover:translate-y-2 duration-300 hover:scale-100 hover:rounded-3xl hover:bg-gradient-to-tr from-pink-400 to-red-400 mt-10 lg:m-0 lg:w-[120px] bg-gray-800 text-lg text-gray-200 px-4 py-2'>Resume</button>
+            </div>
+
+          </div>
+
+          
+          {/* */}
+
+        </motion.div>
+
+        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className=' py-14 px-20 my-10 ml-10 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
+          <div className=" text-zinc-800 font-sans">
+            <div className='lg:flex ml-2 lg:flex-col'>
+              <FaTwitter size={100}/>
+              <button type='button' onClick={()=> router.push('/twitter')} className='transition-all ease-in-out shadow-xl hover:translate-y-2 duration-300 hover:scale-100 hover:bg-gradient-to-tr from-pink-400 to-red-400 mt-10 lg:m-0 lg:w-[120px] bg-gray-800 text-lg text-gray-200 px-4 py-2'><FaTwitter/></button>
+            </div>
+
+          </div>
+
+          
+          {/* */}
+
+        </motion.div>
+       
       </div>
       {/* Projects Section */}
-      <div className=' min-h-[100vh]'>
-        <Projects />
-      </div>
-      <div className=' min-h-[80vh]'>
+      
+      <div>
         <Contact />
       </div>
 
---
+++ pages/resume.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Home.module.css';
+
+
+export default function Resume() {
+    return(
+        <div>
+            <h1>Welcome to my resume.</h1>
+        </div>
+    )
+}
