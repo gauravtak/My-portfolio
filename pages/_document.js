9859b70778a151a098886ef4fb7e53fd783248e6
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-02-25 19:09:46 +0530

    google fonts link added

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link href="https://fonts.googleapis.com/css2?family=Comfortaa&family=Labrada:wght@500&family=Rubik:wght@600&display=swap" rel="stylesheet"></link>
       <body>
         <Main />
         <NextScript />
