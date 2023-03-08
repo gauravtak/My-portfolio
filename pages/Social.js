ce060ef2c80313b313526eda10ff9bf3ed203fce
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-03-08 14:25:13 +0530

    added social links

--- pages/Social.js
+++ pages/Social.js
@@ -6,6 +6,7 @@ import { FaInstagram } from "react-icons/fa"
 import { useEffect } from 'react';
 import { useInView } from "react-intersection-observer";
 import { motion, useAnimation } from "framer-motion";
+import Link from 'next/link';
 
 const boxVariant = {
     visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
@@ -28,19 +29,19 @@ export default function Social() {
     return (
         <motion.div ref={ref} variants={boxVariant} initial='hidden' animate={control} className='flex gap-7 justify-center overflow-x-auto'>
             <div className='px-1 py-1 md:py-4 md:px-4 md:gap-10  md:m-10 sm:py-4 sm:px-4 sm:gap-4 sm:m-4  bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
-                <a href=""><FaTwitter size={20} /></a>
+                <Link href={'https://twitter.com/ALLOY992'}><FaTwitter size={20} /></Link>
             </div>
             <div className='px-1 py-1 md:py-4 md:px-4 md:gap-10  md:m-10 sm:py-4 sm:px-4 sm:gap-4 sm:m-4  bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
-                <a href=""><FaGithub size={20} /></a>
+                <Link href={'https://github.com/gaurav-2-0-0-2'}><FaGithub size={20} /></Link>
             </div>
             <div className='px-1 py-1 md:py-4 md:px-4 md:gap-10  md:m-10 sm:py-4 sm:px-4 sm:gap-4 sm:m-4  bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
-                <a href=""><FaInstagram size={20} /></a>
+                <Link href={'https://www.instagram.com/1_gauravtak/'}><FaInstagram size={20} /></Link>
             </div>
             <div className='px-1 py-1 md:py-4 md:px-4 md:gap-10  md:m-10 sm:py-4 sm:px-4 sm:gap-4 sm:m-4  bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
-                <a href=""><FaLinkedin size={20} /></a>
+                <Link href={'https://www.linkedin.com/in/gaurav-tak-a4190222a/'}><FaLinkedin size={20} /></Link>
             </div>
             <div className='px-1 py-1 md:py-4 md:px-4 md:gap-10  md:m-10 sm:py-4 sm:px-4 sm:gap-4 sm:m-4  bg-gradient-to-tr from-blue-200 to-red-200  rounded-lg'>
-                <a href=""><FaTelegram size={20} /></a>
+                <Link href={'https://web.telegram.org/z/'}><FaTelegram size={20} /></Link>
             </div>
         </motion.div>
     )
