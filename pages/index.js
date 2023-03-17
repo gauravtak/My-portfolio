ab71f4c3ab3601fb1994718212bef3418e177a97
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-03-17 19:21:53 +0530

    scroll text added

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,13 @@ import { useInView } from "react-intersection-observer";
 import { motion, useAnimation } from "framer-motion";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { FaTwitter, FaGit, FaJsSquare, FaReact, FaPython } from "react-icons/fa"
+import { FaTwitter, FaGit, FaJsSquare, FaReact, FaPython } from "react-icons/fa";
 import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
 import { DiMongodb, DiNodejsSmall, DiStackoverflow } from "react-icons/di";
 import { HiArrowCircleRight } from "react-icons/hi";
+import { HiArrowCircleDown } from "react-icons/hi";
 import Header from './Header';
-import {Amatic_SC} from '@next/font/google';
+import { Amatic_SC } from '@next/font/google';
 import { Rubik } from '@next/font/google';
 
 // https://rxresu.me/gauravtak996/gaurav-tak //Resume link
@@ -23,7 +24,7 @@ const amatic = Amatic_SC({
 const rubik = Rubik({
   subsets: ['latin'],
   weight: ['400', '700']
-}) 
+})
 
 const boxVariant = {
   visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
@@ -36,11 +37,14 @@ export default function Home() {
 
   const control = useAnimation();
 
+  const scroll = useAnimation();
+
   const [ref, inView] = useInView();
 
   useEffect(() => {
     if (inView) {
       control.start("visible")
+      scroll.start("hidden")
     }
     // else {
     //   control.start("hidden")
@@ -52,15 +56,20 @@ export default function Home() {
 
 
   return (
-    <div className='lg:ml-20 md:ml-2'>
-      <Header/>
+    <div className='lg:ml-20 md:ml-2 relative'>
+      <Header />
 
+      <motion.div ref={ref} variants={boxVariant} initial="visible" animate={scroll} className='absolute right-[45%] top-[20%]'>
+        <h1 className=' bg-gradient-to-r from-red-400 via-fuchsia-300 to bg-cyan-600 text-transparent bg-clip-text mb-10  font-bold text-5xl '>Scroll</h1>
+        <HiArrowCircleDown className='ml-7 animate-bounce text-white' size={60} />
+
+      </motion.div>
 
       <div className='flex flex-col md:flex-row text-center mt-10 md:text-left min-h-[55vh] md:ml-[100px]  '>
 
         {/*------------------------------------------ 1st Motion div --------------------------------------------------------- */}
-       {/* <div className='transition-all duration-200 hover:scale-125'> */}
-       <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className='card basis-[100%] lg:basis-[50%] md:basis-[37%] py-12 px-10 m-10 md:mx-1 md:mb-10 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg '>
+        {/* <div className='transition-all duration-200 hover:scale-125'> */}
+        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className='hover:drop-shadow-xl card basis-[100%] lg:basis-[50%] md:basis-[37%] py-12 px-10 m-10 md:mx-1 md:mb-10 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg '>
           <div className=" text-zinc-800 font-sans ">
             <img className='transition-all duration-200 hover:scale-150 shadow-2xl my-4 md:my-2 md:ml-4 mx-auto w-20 rounded-full' src="https://www.meme-arsenal.com/memes/5ee99d558641255d6e670bbd54953397.jpg" alt="flower" />
             <div className='md:flex ml-5 md:flex-col'>
@@ -70,23 +79,23 @@ export default function Home() {
 
           </div>
         </motion.div>
-       {/* </div> */}
-       
+        {/* </div> */}
+
 
         {/*---------------------------------------- 2nd Motion div ----------------------------------------------*/}
 
-        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className=' mx-10 py-12 md:py-10 md:px-12 md:my-10 md:mx-5 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
+        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className='hover:drop-shadow-xl  mx-10 py-12 md:py-10 md:px-12 md:my-10 md:mx-5 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
           <div className=" text-zinc-800 font-sans inline-block md:mt-10">
             <div className='md:flex ml-2 md:flex-col '>
-            <div className=' transition-all duration-200 hover:scale-110'>
-            <HiOutlineClipboardDocumentList className='mx-auto' size={80} />
-              <h1 className=' mb-5 md:pl-4 text-2xl font-bold font-Comfortaa'>See My Resume</h1>
-            </div>
-              
+              <div className=' transition-all duration-200 hover:scale-110'>
+                <HiOutlineClipboardDocumentList className='mx-auto' size={80} />
+                <h1 className=' mb-5 md:pl-4 text-2xl font-bold font-Comfortaa'>See My Resume</h1>
+              </div>
+
 
               {/* <button type='button' onClick={() => router.push('/resume')} className='transition-all ease-in-out shadow-xl rounded-xl duration-300 hover:rounded-3xl hover:bg-gradient-to-tr from-pink-400 to-red-400 md:m-0  md:mx-auto md:w-[120px] bg-gray-800 text-lg text-gray-200 px-4 py-2'>Resume</button> */}
               <Link href={"https://drive.google.com/file/d/175EhLdILa8qjbJ5DReww7TrzKsKpc8zj/view?usp=share_link"}><button type='button' className='font-Comfortaa transition-all ease-in-out shadow-xl rounded-xl duration-300 hover:rounded-3xl hover:bg-gradient-to-tr from-pink-400 to-red-400 md:m-0  md:mx-14 md:w-[120px] bg-gray-800 text-lg text-gray-200 px-4 py-2'>Resume</button></Link>
-              
+
 
             </div>
 
@@ -97,14 +106,14 @@ export default function Home() {
 
         {/*---------------------------------------- 3rd Motion div  -------------------------------------------*/}
 
-        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className=' mx-10 py-14 md:py-14 md:px-20 my-10 md:mx-1 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
+        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className=' hover:drop-shadow-xl mx-10 py-14 md:py-14 md:px-20 my-10 md:mx-1 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
           <div className="text-zinc-800">
-            <div className='relative flex ml-40 md:ml-2 md:flex-col md:p-[1rem] sm:inline-block sm:mx-auto'> 
-             {/* ssm:mx-24 */}
+            <div className='relative flex ml-40 md:ml-2 md:flex-col md:p-[1rem] sm:inline-block sm:mx-auto'>
+              {/* ssm:mx-24 */}
               <Link href={'https://twitter.com/ALLOY992'}><FaTwitter className=' md:mt-6 transition-all duration-200 hover:scale-110 sm:w-[5.5rem]' size={100} /></Link>
-              <Link href={'https://twitter.com/ALLOY992'}><button type='button'><HiArrowCircleRight className='hover:animate-spin absolute right-4 top-[5rem]  md:left-[7rem] md:top-[10rem] sm:right-[0.5rem] sm:mt-4' size={60}/></button></Link>
+              <Link href={'https://twitter.com/ALLOY992'}><button type='button'><HiArrowCircleRight className='hover:animate-spin absolute right-4 top-[5rem]  md:left-[7rem] md:top-[10rem] sm:right-[0.5rem] sm:mt-4' size={60} /></button></Link>
             </div>
-           
+
           </div>
 
         </motion.div>
@@ -113,23 +122,23 @@ export default function Home() {
       </div>
 
       {/* -----------------------------------Next Section------------------------------------------ */}
-      
+
 
       <section className='flex flex-col md:flex-row text-center md:text-left min-h-[55vh] md:ml-[100px] '>
-       {/*------------------------------ Things I know Section------------------------------------------------------  */}
-        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className=' basis-[100%] md:basis-[50%] md:pt-40 py-12 px-6 mx-10 md:mb-10 md:mx-1 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
-        {/* ssm:pl-2 */}
+        {/*------------------------------ Things I know Section------------------------------------------------------  */}
+        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className='hover:drop-shadow-xl  basis-[100%] md:basis-[50%] md:pt-40 py-12 px-6 mx-10 md:mb-10 md:mx-1 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
+          {/* ssm:pl-2 */}
           <div className=" text-zinc-800 font-sans">
             <div className='md:flex ml-5 md:flex-col mb-7'>
               <h1 className='md:mt-5 text-lg font-Comfortaa'>Things I Know</h1>
             </div>
             <div className='ml-4 md:ml-[20px] sm:ml-[0.1px] flex md:flex-row mx-auto gap-4 mmd:ml-[29rem]'>
-              <Link  href={'https://developer.mozilla.org/en-US/docs/Web/JavaScript'}><FaJsSquare className='sm:w-[1.6rem]' size={40} /></Link>
-              <Link  href={'https://reactjs.org/'}><FaReact className='sm:w-[1.6rem]' size={40} /></Link>
-              <Link  href={'https://git-scm.com/'}><FaGit className='sm:w-[1.6rem]' size={40} /></Link>
-              <Link  href={'https://www.python.org/'}><FaPython className='sm:w-[1.6rem]' size={40} /></Link>
-              <Link  href={'https://www.mongodb.com/'}><DiMongodb className='sm:w-[1.6rem]' size={40} /></Link>
-              <Link  href={'https://nodejs.org/en/'}><DiNodejsSmall className='sm:w-[1.6rem]' size={40} /></Link>
+              <Link href={'https://developer.mozilla.org/en-US/docs/Web/JavaScript'}><FaJsSquare className='sm:w-[1.6rem]' size={40} /></Link>
+              <Link href={'https://reactjs.org/'}><FaReact className='sm:w-[1.6rem]' size={40} /></Link>
+              <Link href={'https://git-scm.com/'}><FaGit className='sm:w-[1.6rem]' size={40} /></Link>
+              <Link href={'https://www.python.org/'}><FaPython className='sm:w-[1.6rem]' size={40} /></Link>
+              <Link href={'https://www.mongodb.com/'}><DiMongodb className='sm:w-[1.6rem]' size={40} /></Link>
+              <Link href={'https://nodejs.org/en/'}><DiNodejsSmall className='sm:w-[1.6rem]' size={40} /></Link>
 
 
             </div>
@@ -137,16 +146,16 @@ export default function Home() {
           </div>
         </motion.div>
 
-            {/*----------------------------------------------- Hire Me section ------------------------------------------------------- */}
+        {/*----------------------------------------------- Hire Me section ------------------------------------------------------- */}
 
-        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className='basis-[100%] lg:basis-[37.3%] md:basis-[38%] md:pt-20 py-16 px-10 my-10 md:my-0 mx-10 md:mb-10 md:mx-7 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
+        <motion.div ref={ref} variants={boxVariant} initial="hidden" animate={control} className='hover:drop-shadow-xl basis-[100%] lg:basis-[37.3%] md:basis-[38%] md:pt-20 py-16 px-10 my-10 md:my-0 mx-10 md:mb-10 md:mx-7 bg-gradient-to-tr from-pink-200 via-violet-300 to-red-300 rounded-lg'>
           <div className=" text-zinc-800 font-sans sm:pb-6">
             <div className='md:flex ml-5 md:flex-col mb-7'>
               <h1 className='md:mt-5 mr-7 text-2xl font-Comfortaa'>Hire Me</h1>
             </div>
             <div className='relative flex md:flex-row'>
               <h1 className='text-bolder md:text-5xl inline-block md:ml-4 mx-auto text-3xl font-Comfortaa'>Get In Touch</h1>
-              <Link href={"/contact"}><button type='button'><HiArrowCircleRight className=' hover:animate-spin absolute left-[18rem] lg:left-[29rem]  top-[2rem] md:left-[18.7rem] md:top-[1rem] sm:w-[10rem] sm:left-[1.5rem] sm:top-[3rem] mmd:left-[30rem]' size={60}/></button></Link>
+              <Link href={"/contact"}><button type='button'><HiArrowCircleRight className=' hover:animate-spin absolute left-[18rem] lg:left-[29rem]  top-[2rem] md:left-[18.7rem] md:top-[1rem] sm:w-[10rem] sm:left-[1.5rem] sm:top-[3rem] mmd:left-[30rem]' size={60} /></button></Link>
             </div>
             {/* ssm:left-[10.5rem] */}
 
