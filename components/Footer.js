c9dc0dd9987ac27aaf8daeb3099884287d183618
Gaurav Tak <gauravtak996@gmail.com>
2024-04-06 23:00:24 +0530

    remove copyright

--- components/Footer.js
+++ components/Footer.js
@@ -22,7 +22,7 @@ export default function Contact() {
       <Social/>
 
       <section className='md:flex my-4 text-center md:justify-center md:gap-4 font-Comfortaa'>
-        <h2 className=' text-gray-400 mt-16 mb-6 md:my-0'>© {year}</h2>
+        <h2 className=' text-gray-400 mt-16 mb-6 md:my-0'>{year}</h2>
         <p className=' text-gray-400 mb-7'>Created by <span className='hover:text-cyan-400'>Gaurav Tak</span></p>
         <span className='mr-2 md:mr-0 text-white'>Inspired by</span><Link href={"https://gridfolio.framer.website/"} className="text-white">Gridfolio</Link>
       </section>
