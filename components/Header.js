d740453aeaa20b915a148fd25423ac0e41d90f58
Gaurav Tak <gauravtak996@gmail.com>
2024-10-20 23:54:53 +0530

    change navbar to sidenavbar

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import { MdHome } from "react-icons/md";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { RiProjectorLine } from "react-icons/ri";
 
 export default function Header() {
     return(
-        <div className='font-JetBrainsMono mt-2'>
-        	<ul className='flex flex-row justify-center'>
-        	  <Link href="/"><li className='ml-3 p-2 text-white hover:text-cyan-500  font-extrabold text-[15px] md:text-[20px]' >HOME</li></Link>
-        	  <Link href="/projects"><li className='ml-3 p-2 text-white hover:text-cyan-500 font-extrabold text-[15px] md:text-[20px]' >PROJECTS</li></Link>
-        	  <Link href="/career"><li className='ml-3 p-2 text-white hover:text-cyan-500 font-extrabold text-[15px] md:text-[20px]'>CAREER</li></Link>
+        <div className='font-JetBrainsMono mt-7 fixed top-0 left-0 h-full w-[160px]  z-[2]'>
+        	<ul className='flex flex-col gap-2 '>
+        	  <Link href="/"><li className='ml-3 p-2 text-white hover:text-cyan-500  font-extrabold text-[15px] md:text-[20px]' ><MdHome/></li></Link>
+        	  <Link href="/projects"><li className='ml-3 p-2 text-white hover:text-cyan-500 font-extrabold text-[15px] md:text-[20px]'><RiProjectorLine/></li></Link>
+        	  <Link href="/career"><li className='ml-3 p-2 text-white hover:text-cyan-500 font-extrabold text-[15px] md:text-[20px]'><MdOutlineWorkOutline/></li></Link>
         	</ul>
       </div>
     )
