7b2e927e678e4b9f34006ce2c2ab5fc7a8275444
Gaurav Tak <gauravtak996@gmail.com>
2024-12-17 19:29:33 +0530

    adding a books section

--- components/Home/Item.js
+++ components/Home/Item.js
@@ -1,11 +1,11 @@
 import { GoLinkExternal } from "react-icons/go";
 
-export default function Project({id, title, desc, githubLink}){
+export default function Item({id, title, desc, link}){
     return(
         <div className="project-card rounded-xl px-4 py-6">
             <div className="flex items-center justify-between" >
                 <h1 className="font-bold text-xl">{title}</h1> 
-                <a href={githubLink}>
+                <a href={link}>
                     <GoLinkExternal/>
                 </a>
             </div> 
